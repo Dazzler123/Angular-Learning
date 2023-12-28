274418b6dead8026e2adffcd22d2e0a79782f3b3
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-12-28 20:48:28 +0530

    Introduced Event binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,18 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector:'app-home',
-  templateUrl:'./home.component.html',
-  styleUrls:['./home.component.scss']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-    width="400";
-    image='https://www.google.com/imgres?imgurl=https%3A%2F%2Frepository-images.githubusercontent.com%2F314186076%2F79a18fff-1aeb-4a12-9340-db81b383b5e7&tbnid=G48fTm7qth7dNM&vet=12ahUKEwio1qDSs7KDAxVHSGwGHcBjBkIQMygBegQIARB1..i&imgrefurl=https%3A%2F%2Fgithub.com%2Ftopics%2Fangular-architecture&docid=sz96cIfbH0BQ1M&w=1600&h=840&q=angular%20images&ved=2ahUKEwio1qDSs7KDAxVHSGwGHcBjBkIQMygBegQIARB1';
+  data: string = "";
+
+  setData(value: string): void {
+    this.data = value;
+  }
+
+  printData(): void {
+    console.log(this.data);
+  }
 }
