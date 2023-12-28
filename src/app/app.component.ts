48237e17cc84bb8d6fe4002ebc4520643b714174
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-12-28 22:13:59 +0530

    Navigating to pages (routing) using typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app1';
+
+  constructor(private router: Router) {
+
+  }
+
+  navigateToPage(pathSegment: string): void {
+    this.router.navigateByUrl(pathSegment)
+      .then(e => {
+        console.log(e);
+      }).catch(error => {
+      console.log(error);
+    })
+  }
 }
