5d1f85d159f6e7654bd1c56930a7342fb484089e
Dasindu Hewagamage <dasinduhewagamage@gmail.com>
2023-12-28 21:07:47 +0530

    Introduced Two way data bind

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,4 @@ import {Component} from "@angular/core";
 })
 export class HomeComponent {
   data: string = "";
-
-  setData(value: string): void {
-    alert("Typed Text : " + value);
-  }
-
-  printData(): void {
-    console.log(this.data);
-  }
 }
